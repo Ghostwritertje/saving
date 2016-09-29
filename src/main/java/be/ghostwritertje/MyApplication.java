7fc2c116683ff4a9e7b567635fe659ab9dcb83f8
Jorandeboever <jorandeboever@gmail.com>
2016-09-29 19:16:40 +0200

    Ghostwritertje Directory

--- src/main/java/be/ghostwritertje/MyApplication.java
+++ src/main/java/be/ghostwritertje/MyApplication.java
@@ -1,8 +1,11 @@
+package be.ghostwritertje;
+
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
 
 /**
- * Created by Jorandeboever on 29-Sep-16.
+ * Created by Jorandeboever
+ * Date: 29-Sep-16.
  */
 public class MyApplication extends WebApplication {
 
